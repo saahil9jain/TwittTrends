e0f87fb6102ab51893149163fad994e96cdcdd6b
Lakshmi Bodapati <mounikabodapati@dyn-160-39-242-67.dyn.columbia.edu>
2017-04-07 21:54:22 -0400

    SNS publishing works

--- TwittTrends/worker.py
+++ TwittTrends/worker.py
@@ -5,7 +5,8 @@ import boto3
 import json
 from watson_developer_cloud import AlchemyLanguageV1
 from concurrent.futures import ThreadPoolExecutor
-
+import boto.sqs
+from boto.sqs.message import Message
 
 arn = 'arn:aws:sns:us-east-1:332176844987:TwittTrends'
 queueName = 'TwittTrends'
@@ -14,12 +15,13 @@ alchemyapi = AlchemyLanguageV1(api_key='bb4bc7f058004c73d08b9983fc76e84af3701e0f
 thread_waittime =  10
 working = 10
 
-sqs = boto3.resource('sqs')
 sns = boto3.resource('sns')
 
 # Get the queue. This returns an SQS.Queue instance
-queue = sqs.Queue('https://sqs.us-east-1.amazonaws.com/332176844987/TwittTrends')
 endpoint = sns.PlatformEndpoint(arn)
+
+conn = boto.sqs.connect_to_region("us-east-1", aws_access_key_id='AKIAJ2SBRP7FL2443UCA', aws_secret_access_key='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')
+queue = conn.create_queue('TwittTrends')
 
 
 
@@ -28,25 +30,20 @@ def parse_tweet():
     print("sleep")
     #sleep(working)
     print("done sleep")
-    for message in queue.receive_messages(MessageAttributeNames=['All'], VisibilityTimeout=30, MaxNumberOfMessages=1):
-        print("moo")
-        if message.body is not None:
-            print(message.body)
-            tweet_json = json.loads(message.body)
-            print("moo1")
+    for message in queue.get_messages(1):
+        if message.get_body() is not None:
+            tweet_json = json.loads(message.get_body())
             snsMsg = {}
             ## geo
             coordinates = tweet_json['coordinates']
             tweet_text = tweet_json['text']
-            print(tweet_text)
             response = alchemyapi.sentiment(text=tweet_text)
-            print(response)
             if response['status'] == 'OK':
 
                 sentimentResult = response["docSentiment"]
 
                 snsMsg['sentiment'] = sentimentResult
-                snsMsg['tweet'] = message.body
+                snsMsg['tweet'] = tweet_text
                 snsMsg['coordinates'] = coordinates
                 ## dump snsMsg and send message to SNS
                 snsMessage = json.dumps(snsMsg)
