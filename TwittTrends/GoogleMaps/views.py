63fd48c86c146010d66aad1c7471670492fc5705
Lakshmi Bodapati <mounikabodapati@dyn-160-39-242-67.dyn.columbia.edu>
2017-04-09 13:17:19 -0400

    reformatting

--- TwittTrends/GoogleMaps/views.py
+++ TwittTrends/GoogleMaps/views.py
@@ -1,4 +1,7 @@
-#GoogleMaps/views.py
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# GoogleMaps/views.py
+
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.template import RequestContext, loader
@@ -9,16 +12,19 @@ from django.views.decorators.csrf import csrf_exempt
 import requests
 import json
 
+
 def index(request):
-	return render(request, 'index.html')
+    return render(request, 'index.html')
+
 
 def post(Request):
-    if Request.method == "POST":
+    if Request.method == 'POST':
         msg = Request.POST.get('Search', None)
-    if Request.method == "GET":
+    if Request.method == 'GET':
         msg = Request.GET.get('Search', None)
-    elastic = 'http://search-twitter-map-obukguehsa2d4it32tto6i3vbm.us-east-1.es.amazonaws.com/twittertrend/_search?q='
-    response = requests.get(elastic+msg+"&size=100")
+    elastic = \
+        'http://search-twitter-map-obukguehsa2d4it32tto6i3vbm.us-east-1.es.amazonaws.com/twittertrend/_search?q='
+    response = requests.get(elastic + msg + '&size=100')
     json_response = json.loads(response.text)
     tweet = []
     coordinates = []
@@ -27,33 +33,34 @@ def post(Request):
         tweet.append(resp['_source']['text'])
         coordinates.append(resp['_source']['coordinates'])
         sentiments.append(resp['_source']['sentiment']['type'])
-    data = {'coordinates': coordinates, 'tweets': tweet, 'sentiments': sentiments}
-    print(data)
+    data = {'coordinates': coordinates, 'tweets': tweet,
+            'sentiments': sentiments}
+    print (data)
     return JsonResponse(data)
+
 
 @csrf_exempt
 def snspoll(request):
-    context={"message":"confirmation"}
-    if request.method == "GET":
-        return render(request,'index.html')
+    context = {'message': 'confirmation'}
+    if request.method == 'GET':
+        return render(request, 'index.html')
     else:
-		body_unicode = request.body.decode('utf-8')
-        header=json.loads(body_unicode)
-        if header['Type']=="SubscriptionConfirmation":
-            subscribleURL=header['SubscribeURL']
+        body_unicode = request.body.decode('utf-8')
+        header = json.loads(body_unicode)
+        if header['Type'] == 'SubscriptionConfirmation':
+            subscribleURL = header['SubscribeURL']
             urllib2.urlopen(subscribleURL).read()
-        elif header['Type']=="Notification":
-            message=json.loads(json.loads(header["Message"]).get('default'))
-            tweet=message['tweet']
-            lat=message['lat']
-            lon=message['lon']
-            sentiment=message['sentiment']['type']
+        elif header['Type'] == 'Notification':
+            message = json.loads(json.loads(header['Message'
+                                 ]).get('default'))
+            tweet = message['tweet']
+            lat = message['lat']
+            lon = message['lon']
+            sentiment = message['sentiment']['type']
 
-            tweet_data={
-                "tweet":tweet,
-                "coordinates":{"lat":lat,"lon":lon},
-                "sentiment":sentiment
-            }
-            requests.post("search-twitter-map-obukguehsa2d4it32tto6i3vbm.us-east-1.es.amazonaws.com/twittertrend/tweets",json=tweet_data)
-            context={"message": "notification"}
-    return render(request,'index.html',context)
+            tweet_data = {'tweet': tweet, 'coordinates': {'lat': lat,
+                          'lon': lon}, 'sentiment': sentiment}
+            requests.post('search-twitter-map-obukguehsa2d4it32tto6i3vbm.us-east-1.es.amazonaws.com/twittertrend/tweets'
+                          , json=tweet_data)
+            context = {'message': 'notification'}
+    return render(request, 'index.html', context)
