14186af70f55332d2b79e554508ab125b75dade8
Lakshmi Bodapati <mounikabodapati@dyn-160-39-242-67.dyn.columbia.edu>
2017-04-09 22:08:00 -0400

    worker app

---
+++ worker/worker_app/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render
+from time import sleep
+import boto3
+import json
+from watson_developer_cloud import AlchemyLanguageV1
+from concurrent.futures import ThreadPoolExecutor
+import boto.sqs
+from boto.sqs.message import Message
+from django.http import HttpResponse, request, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+
+
+arn = 'arn:aws:sns:us-east-1:332176844987:TwittTrends'
+queueName = 'TwittTrends'
+alchemyapi = AlchemyLanguageV1(api_key='bb4bc7f058004c73d08b9983fc76e84af3701e0f')
+
+thread_waittime =  20
+working = 10
+
+# SNS
+sns = boto3.resource('sns')
+endpoint = sns.PlatformEndpoint(arn)
+# SQS
+conn = boto.sqs.connect_to_region("us-east-1", aws_access_key_id='AKIAJ2SBRP7FL2443UCA', aws_secret_access_key='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')
+queue = conn.create_queue('TwittTrends')
+
+def parse_tweet():
+    sleep(working)
+    for message in queue.get_messages(1):
+        if message.get_body() is not None:
+            tweet_json = json.loads(message.get_body())
+            snsMsg = {}
+            ## parse tweet for location, text, and sentiment
+            coordinates = tweet_json['coordinates']
+            tweet_text = tweet_json['text']
+            response = alchemyapi.sentiment(text=tweet_text)
+            if response['status'] == 'OK':
+
+                sentimentResult = response["docSentiment"]["type"]
+
+                snsMsg['sentiment'] = sentimentResult
+                snsMsg['tweet'] = tweet_text
+                snsMsg['coordinates'] = coordinates
+                # Dump snsMsg and send message to SNS
+                snsMessage = json.dumps(snsMsg)
+                print(snsMessage)
+                response = endpoint.publish(Message=snsMessage,Subject='TwittTrends')
+        # Let the queue know that the message is processed
+        message.delete()
+
+@csrf_exempt
+def index(request):
+    if request.method == 'POST':
+        parse_tweet()
+        '''executor = ThreadPoolExecutor(max_workers=7)
+        while True:
+            executor.submit(parse_tweet)
+            sleep(thread_waittime)'''
+    response = {"response":"thread finished"}
+    return JsonResponse(response)
